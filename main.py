d71ca1556e52f294eb0c0c8d3be084413dfa3631
WinterCollinsSchool <winterc6@nycstudents.net>
2025-12-19 12:44:22 -0500

    Fully switched to CTk and completed saving
    huge

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-from tkinter import *
-from tkmacosx import *
+import customtkinter
 
-root=Tk()
+root=customtkinter.CTk()
 root.title("Word Prosesser")
 
 root.geometry("300x150")
 
 def save_as():
-	new_file_name=input("Save as:")
-	clean_file_name=new_file_name+".txt"
+	new_file_name=customtkinter.CTkInputDialog(text="Save as:", title="Save")
+	clean_file_name=new_file_name.get_input()+".txt"
 	text=text_box.get("1.0", "end-1c")
 	f = open(clean_file_name,"x")
 	with open(clean_file_name, "w") as f:
 		f.write(text)
 	with open(clean_file_name) as f:
 		print(f.read())
-	self.distroy()
+	root.destroy()
 
-save=Button(root,text="Save",command = lambda:save_as())
-label = Label(root, text="Write your notes here!")
-text_box = Text(root,height=15,width=50)
+save=customtkinter.CTkButton(root,text="Save",command = lambda:save_as())
+label = customtkinter.CTkLabel(root, text="Write your notes here!")
+text_box = customtkinter.CTkTextbox(root,height=250,width=500)
 
 label.grid(row=0,column=0,pady=5,padx=10)
 text_box.grid(row=1,column=0, columnspan=2,padx=5)
