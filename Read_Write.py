490a3f76a982e061c1aa793d40dd938bb283eeb0
WinterCollinsSchool <winterc6@nycstudents.net>
2026-01-09 12:37:32 -0500

    Update Read_Write.py

--- Read_Write.py
+++ Read_Write.py
@@ -6,5 +6,9 @@ app.title("Simple CTkinter App")
 app.geometry("300x150")
 
 text_box = customtkinter.CTkTextbox(app)
+write=customtkinter.CTkButton(app,text='Write To File')
+read=customtkinter.CTkButton(app,text='Read From File')
 
 text_box.pack()
+write.pack()
+read.pack()
