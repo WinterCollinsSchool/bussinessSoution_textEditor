7930c0137919dda30ba36af9d6020b411c7c19f0
WinterCollinsSchool <winterc6@nycstudents.net>
2026-01-15 11:55:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ root.geometry("300x150")
 def load_from():
     old_file_name = customtkinter.CTkInputDialog(text="What file?", title="Load")
     clean_old_file_name = old_file_name.get_input() + ".txt"
-    with open(clean_old_file_name) as f:
+    with open("notes/"+clean_old_file_name) as f:
         text_box.insert("0.0", f.read())
 
 def save_as():
@@ -26,14 +26,14 @@ root.grid_columnconfigure(1, weight=1)
 root.grid_columnconfigure(2, weight=1)
 root.grid_rowconfigure(1, weight=1)
 
-save_asb = customtkinter.CTkButton(root, text="Save", command=lambda: save_as())
+save = customtkinter.CTkButton(root, text="Save", command=lambda: save_as())
 label = customtkinter.CTkLabel(root, text="Write your notes here!")
 text_box = customtkinter.CTkTextbox(root, height=500, width=500)
 load = customtkinter.CTkButton(root,text="Load",command=lambda:load_from())
 
 label.grid(row=0, column=0, pady=5,sticky="w", padx=5)
 text_box.grid(row=1, column=0, columnspan=3, padx=5,sticky="news")
-save_asb.grid(row=0, column=1, padx=10, sticky="we")
+save.grid(row=0, column=1, padx=10, sticky="we")
 load.grid(row=0,column=2,padx=10,sticky="w")
 
 root.mainloop()
